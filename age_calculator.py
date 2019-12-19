a826994618706e0eb033b5ba16d41199e0fa7901
lailalelouch <lsb-95@hotmail.com>
2019-12-19 13:57:17 +0300

    added test fiile

---
+++ age_calculator.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+def check_birthdate(year, month, day):
+	# write code here
+
+def calculate_age(year, month, day):
+    # write code here
+
+def main():
+	# write main code here
+
+
+if __name__ == '__main__':
+	main()
